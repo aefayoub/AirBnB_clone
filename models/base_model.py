f228ef110c6de56493988e13f8b4928b95e3a126
aefayoub <aef.ayoub@gmail.com>
2023-10-11 07:38:11 -0700

    cmt all

--- models/base_model.py
+++ models/base_model.py
@@ -4,6 +4,7 @@ Imports modules
 """
 import uuid
 from datetime import datetime
+from models import storage
 
 """
 Module that contains class Base
@@ -19,7 +20,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.update_at = self.created_at
-            
+            storage.new(self)
         else:
             f = "%Y-%m-%dT%H:%M:%S.%f"
             for key, value in kwargs.items():
@@ -28,19 +29,17 @@ class BaseModel:
                 if key != '__class__':
                     setattr(self, key, value)
 
-            self.id = Base.__nb_objects
-
     def __str__(self):
         """function str print: [<class name>] (<self.id>) <self.__dict__> """
         className = "[" + self.__class__.__name__ + "]"
         classDict = {k: v for (k, v) in self.__dict__.items() if (not v) is False}
-        classDict = self.__dict__
+        #classDict = self.__dict__
         return className + " (" + self.id + ") " + str(classDict)
 
     def save(self):
         """Updates the public instance attribute"""
         self.updated_at = datetime.now()
-        #storage.save()
+        storage.save()
 
     def to_dict(self):
         """returns a dictionary containing all keys/values of __dict__ of the instance"""
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -6,10 +6,10 @@ import os
 """Module FileStorage that serializes instances to a JSON file"""
 
 
-class Filestorage:
+class FileStorage:
     """class FileStorage that serializes instances to a JSON file """
     __file_path = "file.json"
-    __Objects = {}
+    __objects = {}
 
     def all(self):
         """returns the dictionary __objects"""
@@ -25,18 +25,18 @@ class Filestorage:
         dictionary = {}
 
         for k, v in FileStorage.__objects.items():
-            dictionary[k] = value.todict()
+            dictionary[k] = v.to_dict()
 
-        with open(FileStorage.__file_path,"w") as f:
+        with open(FileStorage.__file_path,'w') as f:
             json.dump(dictionary, f)
 
     def reload(self):
         """Writing the dictionary representation to a file won’t be relevant"""
         from models.base_model import BaseModel
 
-        alldict = {"BaseModel": BaseModel}
+        alldict = {'BaseModel': BaseModel}
 
         if os.path.exists(FileStorage.__file_path) is True:
-            with open(FIleStorage.__file_path, "r") as f:
+            with open(FileStorage.__file_path, 'r') as f:
                 for k, v in json.load(f).items():
-                    self.new(alldict[v["__class__"]](**v)
+                    self.new(alldict[v["__class__"]](**v))
