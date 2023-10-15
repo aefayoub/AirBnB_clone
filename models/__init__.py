e140cde049feb9f5183566266f23e613775526d6
aefayoub <aef.ayoub@gmail.com>
2023-10-15 12:39:50 -0700

    cmt init file

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
+
 """import all modules and packages needed"""
+
 
 from models.engine.file_storage import FileStorage
 
+# Create an instance of the FileStorage class
 storage = FileStorage()
+
+# Reload any stored data from a previous session
 storage.reload()
