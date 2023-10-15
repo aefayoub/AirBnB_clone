14e533627b47650fb470b3f3418deb64fa352e82
aefayoub <aef.ayoub@gmail.com>
2023-10-15 13:18:11 -0700

    add all models

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -34,6 +34,11 @@ class FileStorage:
         """Writing the dictionary representation to a file won’t be relevant"""
         from models.base_model import BaseModel
         from models.user import User
+        from models.place import Place
+        from models.state import State
+        from models.state import City
+        from models.amenity import Amenity
+        from models.review import Review
 
         alldict = {'BaseModel': BaseModel, 'User': User, 'Place': Place, 'State': State, 'City': City, 'Amenity': Amenity, 'Review': Review}
 
